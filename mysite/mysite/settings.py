82c9d608ab55823eebf0d1ec48b7bd3bda6d1da7
ARIANA MARIA GARCIA REYES <garaariana@gmail.com>
2022-10-08 14:32:14 -0500

    Cambios settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -99,7 +99,11 @@ DATABASES = {
     }
 }
 
-
+if "DATABASE_URL" in os.environ:
+    # Configure Django for DATABASE_URL environment variable.
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
+        
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
